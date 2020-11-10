ad7f15755d012c7c696c5a5652dd8efba935e81f
Manish Purohit <manishkumar9866@gmail.com>
2020-11-10 08:33:52 +0000

    donee

--- src/components/App.js
+++ src/components/App.js
@@ -3,32 +3,43 @@ import "./../styles/App.css";
 import TodosList from "./TodosList";
 
 function App() {
-	const [todosList,updateTodosList] = React.useState([]);
-	const [todo,updateTodo] = React.useState("");
+  const [todosList, updateTodosList] = React.useState([]);
+  const [todo, updateTodo] = React.useState("");
 
-	const handleAdd = ()=>{
-		if(todo==="") return;
-		let unique = todosList;
-		unique.push(todo);
-		unique = [...new Set(unique)];
-		updateTodosList(unique);
-		console.log(todosList)
-		updateTodo("");
-	}
-	const handleChange=(event)=>{
-		updateTodo(event.target.value);
-	}
-	return (
-	<div id="main">
-	
-	<textarea id="task" type="text" onChange={()=>handleChange(event)} value={todo}/>
-	<button id="btn" onClick={()=>handleAdd()}>Add</button>
-	
-	{todosList.length === 0 ? "":<TodosList key="todolist" todoList={todosList} updateTodoList={updateTodosList}/>}
-	
-	</div>
-	);
+  const handleAdd = () => {
+    if (todo === "") return;
+    let list = todosList;
+    list.push(todo);
+    list = [...new Set(list)];
+    updateTodosList(list);
+    updateTodo("");
+  };
+  const handleChange = (event) => {
+    updateTodo(event.target.value);
+  };
+  return (
+    <div id="main">
+      <textarea
+        id="task"
+        type="text"
+        onChange={() => handleChange(event)}
+        value={todo}
+      />
+      <button id="btn" onClick={() => handleAdd()}>
+        Add
+      </button>
+
+      {todosList.length === 0 ? (
+        ""
+      ) : (
+        <TodosList
+          key="todolist"
+          todoList={todosList}
+          updateTodoList={updateTodosList}
+        />
+      )}
+    </div>
+  );
 }
-
 
 export default App;
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,57 +1,87 @@
-
 import React from "react";
 import "./../styles/App.css";
 
 export default function TodosList(props) {
-    let todosList = [...new Set(props.todoList)];
-    let updateTodosList = props.updateTodoList;
-    const [editTodoInput,updateEditTodo] = React.useState("");
-    const changeEditTodoState = (event)=>{
-        let svEle = document.getElementsByClassName(event.target.className)[1];
-        updateEditTodo(event.target.value);
-        
-        
-        
-    }
-    const editTodo = (event)=>{
-        let inputEle = document.getElementsByClassName(event.target.className)[0];
-        let saveEle = document.getElementsByClassName(event.target.className)[1];
-        inputEle.style.display = "block";
-        saveEle.style.display = "block";
-        event.target.style["display"] = "none";
+  let todosList = [...new Set(props.todoList)];
+  let updateTodosList = props.updateTodoList;
+  const [editTodoInput, updateEditTodo] = React.useState("");
+  const changeEditTodoState = (event) => {
+    updateEditTodo(event.target.value);
+  };
+  const editTodo = (event) => {
+    let inputEle = document.getElementsByClassName(event.target.className)[0];
+    let saveEle = document.getElementsByClassName(event.target.className)[1];
+    inputEle.style.display = "block";
+    saveEle.style.display = "block";
+    event.target.style["display"] = "none";
+  };
+  const deleteTodo = (event) => {
+    let target = event.target;
+    let unique = todosList;
+    unique = unique.filter((e, index) => {
+      return index.toString() !== target.className;
+    });
+    updateTodosList(unique);
+  };
+  const saveEditTodo = (event) => {
+    let index = parseInt(event.target.className);
+    let unique = todosList;
+    unique[index] = editTodoInput;
+    let inputEle = document.getElementsByClassName(event.target.className)[0];
+    let saveEle = document.getElementsByClassName(event.target.className)[1];
+    let editEle = document.getElementsByClassName(event.target.className)[2];
+    inputEle.style.display = "none";
+    saveEle.style.display = "none";
+    editEle.style.display = "block";
+    updateTodosList(unique);
+    updateEditTodo("");
+  };
 
-    }
-    const deleteTodo = (event)=>{
-        let target = event.target;
-        let unique = todosList;
-        unique = unique.filter((e,index)=>{return index.toString() !== target.className});
-        updateTodosList(unique);
-    }
-    const saveEditTodo = (event)=>{
-        
-        let index = parseInt(event.target.className);
-        let unique = todosList;
-        unique[index] = editTodoInput;
-        console.log(unique[index]);
-        let inputEle = document.getElementsByClassName(event.target.className)[0];
-        let saveEle = document.getElementsByClassName(event.target.className)[1];
-        let editEle = document.getElementsByClassName(event.target.className)[2];
-        inputEle.style.display = "none";
-        saveEle.style.display = "none";
-        editEle.style.display="block";
-        updateTodosList(unique);
-    }
-    
-    return (
-        <>
-            {todosList.map((e,index)=>{
-                return (<li key={index.toString()} className="list">
-                    {e} 
-                    <textarea className={"editTask",index} style={{display:"none"}} onChange={()=>changeEditTodoState(event)} value={editTodoInput} key={`inp${e}`} type="text" placeholder="Edit here..." /><br/>
-                    <button className={"saveTask",index} onClick={saveEditTodo} style={{display:"none"}} disabled={!editTodoInput} key={`save${e}`} >save</button><br/>
-                    <button className={"edit",index}  key={`edit${index}`} onClick={()=>editTodo(event)}>Edit</button><br/>
-                    <button key={`delete${index}`} className={"delete",index} onClick={()=>deleteTodo(event)}>Delete</button><br/>
-                    </li>)})}
-        </>
+  return (
+    <>
+      {todosList.map((e, index) => {
+        return (
+          <li key={index.toString()} className="list">
+            {e}
+            <textarea
+              className={("editTask", index)}
+              style={{ display: "none" }}
+              onChange={() => changeEditTodoState(event)}
+              value={editTodoInput}
+              key={`inp${index}`}
+              type="text"
+              placeholder="Edit here..."
+            />
+            <br />
+            <button
+              className={("saveTask", index)}
+              onClick={() => saveEditTodo(event)}
+              style={{ display: "none" }}
+              disabled={!editTodoInput}
+              key={`save${index}`}
+            >
+              save
+            </button>
+            <br />
+            <button
+              className={("edit", index)}
+              key={`edit${index}`}
+              onClick={() => editTodo(event)}
+            >
+              Edit
+            </button>
+            <br />
+            <button
+              key={`delete${index}`}
+              className={("delete", index)}
+              onClick={() => deleteTodo(event)}
+            >
+              Delete
+            </button>
+            <br />
+          </li>
         );
-    }
+      })}
+    </>
+  );
+}
