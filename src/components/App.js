08db774f9f5b3ae05d3254e12a73789852a37014
Manish Purohit <manishkumar9866@gmail.com>
2020-11-20 00:08:54 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,48 @@
 import React from "react";
 import "./../styles/App.css";
-import TodosList from "./TodosList";
+import Todos from "./Todos";
 
 function App() {
-  const [todosList, updateTodosList] = React.useState([]);
-  const [todo, updateTodo] = React.useState("");
+	const [todosList,updateTodosList] = React.useState([]);
+	const [todo,updateTodo] = React.useState("");
+	const todoId = React.useRef(0);
+	const handleAdd = ()=>{
+	const todosCopy = [...todosList];
+    todosCopy.push({ id: todoId.current, text: todo });
+    todoId.current = todoId.current + 1;
+	updateTodo("");
+	updateTodosList(todosCopy);
+	}
 
-  const handleAdd = () => {
-    if (todo === "") return;
-    let list = todosList;
-    list.push(todo);
-    list = [...new Set(list)];
-    updateTodosList(list);
-    updateTodo("");
-  };
-  const handleChange = (event) => {
-    updateTodo(event.target.value);
-  };
-  return (
-    <div id="main">
-      <textarea
-        id="task"
-        type="text"
-        onChange={() => handleChange(event)}
-        value={todo}
-      />
-      <button id="btn" onClick={() => handleAdd()}>
-        Add
-      </button>
+	const handleDelete = (id)=>{
+		let todosCopy = [...todosList]
+		todosCopy = todosCopy.filter((el)=>el.id !== id?el:null);
+		updateTodosList(todosCopy);
 
-      {todosList.length === 0 ? (
-        ""
-      ) : (
-        <TodosList
-          key="todolist"
-          todoList={todosList}
-          updateTodoList={updateTodosList}
-        />
-      )}
-    </div>
-  );
+	}
+
+	const saveChangeText = (id,text)=>{
+		let todosCopy = [...todosList];
+		todosCopy.forEach((el)=>{
+			if(id === el.id){
+				el.text = text;
+			}
+		});
+		updateTodosList(todosCopy);
+	}
+	return (
+	<div id="main">
+	
+	<input id="task" type="textarea" onChange={(e)=>updateTodo(e.target.value)} value={todo}/>
+	<button id="btn" onClick={()=>handleAdd()} disabled={!todo}>Add</button>
+	<ul>
+		{todosList.map((todo)=>(
+			<Todos key={todo.id} value={todo} saveChangeText={saveChangeText} handleDelete={handleDelete} />
+	))}
+	</ul>
+	</div>
+	);
 }
+
 
 export default App;
